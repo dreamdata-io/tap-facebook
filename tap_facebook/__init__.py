87ed3848db5d372c4f4e796d9162684163ef5262
JingLin0 <jinglin.daisy@gmail.com>
2023-02-06 12:17:48 +0100

    rename to config account ids

--- tap_facebook/__init__.py
+++ tap_facebook/__init__.py
@@ -28,7 +28,7 @@ def do_sync(account_ids, config, state):
 def main():
     args = singer.utils.parse_args([])
 
-    account_ids = args.config.get("account_ids", [])
+    config_account_ids = args.config.get("account_ids", [])
     access_token = args.config.get("access_token") or os.environ.get(
         "FACEBOOK_ACCESS_TOKEN"
     )
@@ -55,7 +55,7 @@ def main():
     all_account_ids = {accnt["account_id"]: accnt["id"] for accnt in casted_ad_accounts}
     accnt_ids = []
 
-    for account_id in account_ids:
+    for account_id in config_account_ids:
         if account_id not in all_account_ids:
             logger.warn(
                 f"invalid account id: {account_id} not in list of valid account ids: {all_account_ids.keys()}"
